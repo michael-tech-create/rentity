205f46db9da826d62337d79682d906a23cd3fc71
michael-tech-create <mikeunogwu2@gmail.com>
2025-09-24 18:42:31 +0100

    first push

--- app/pra/folder/page.js
+++ app/pra/folder/page.js
@@ -1,23 +1,20 @@
+"use client";
+import { useState } from "react";
 
-    "use client"
-import { useEffect, useState } from "react";
-import { IoLogoFacebook } from "react-icons/io";
+export default function ModalPage() {
+  const [show, setShow] = useState(false);
 
-export default function LoginPage () {
-    const [count, setCount] = useState (0);
-        useEffect (()=>{
-            const handleFetch = async () => {
-                const response = await fetch ("");
-                const data = await response.json();
-                console.log(data);
-            }
-            handleFetch()
-        },[])
-     return (
-        <main className="flex flex-col gap-3 justify-center items-center">
-            < IoLogoFacebook className="text-4xl text-red-500 mt-2" />
-            <p>Count: {count} </p>
-      <button onClick={()=>{setCount(count + 1)}} className="w-[100px] h-[70px] rounded bg-blue-500 text-white">increment</button>
-        </main>
-    );
+  return (
+    <main>
+      <button onClick={() => setShow(true)}>Open Modal</button>
+
+      {show && (
+        <div>
+          <h2>Modal Window</h2>
+          <p>This is a simple modal</p>
+          <button onClick={() => setShow(false)}>Close</button>
+        </div>
+      )}
+    </main>
+  );
 }
